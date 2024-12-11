5444369557f3e9982541d9e00664e44fb49c5d3b
Adriano Skroch <adriano@Mac-mini-de-Adriano.local>
2024-12-11 18:33:12 -0300

    remove ui lib

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,8 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-config.resolver.sourceExts.push('sql'); // <--- add this
-
+config.resolver.sourceExts.push("sql"); // <--- add this
+config.resolver.assetExts.push("ttf");
 module.exports = config;
